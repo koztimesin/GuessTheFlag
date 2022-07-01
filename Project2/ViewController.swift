9b2447889a7e233deeb57ad43e85f6065d9f4356
koztimesin <a.isumairu01@gmail.com>
2022-07-01 12:39:33 +0300

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Karthus Saffron on 7/1/22.
+//  Copyright © 2022 koztimesin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
